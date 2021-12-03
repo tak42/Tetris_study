68251965b73067541a72ae3a27884e4b59a0c111
tak42 <1206ookubo@gmail.com>
2021-12-03 20:45:39 +0900

    アニメーションの確認

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -11,7 +11,7 @@ const Container = styled.div`
   height: 100vh;
   min-height: 100vh;
   padding: 0 0.5rem;
-  background-color: green;
+  background-color: black;
 `
 
 const Main = styled.main`
@@ -36,13 +36,13 @@ const Grid = styled.div`
     width: 100%;
   }
 `
-const Area = styled.div`
+const Area = styled.div<{ val: number }>`
   width: 50px;
   height: 50px;
   border: 1px solid black;
-  background-color: white;
+  background-color: ${(props) => (props.val === 1 ? 'lightBlue' : 'gray')};
 `
-type Cell = { row: number; col: number }
+
 const Home: NextPage = () => {
   // Tetrisの特徴
   // 1.ある一定の時間がたったらブロックが投下される
@@ -51,8 +51,8 @@ const Home: NextPage = () => {
   // 4.クリア行より上の行は１段さがる
   // 5.最上段より上にブロックが来たら負け
   const longRod: number[] = [4]
-  const square: number[] = [1, 2]
   const [timer, setTimer] = useState(0)
+  const [blockRow, setBlockRow] = useState(0)
   // prettier-ignore
   const [field, setField] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -77,7 +77,24 @@ const Home: NextPage = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ])
   const blockDown = useMemo(() => {
-    // timerのカウントごとにブロックを落としていく処理
+    const newField: number[][] = JSON.parse(JSON.stringify(field))
+    for (let n = 0; n < longRod[0]; n++) {
+      newField[n + blockRow][4] = 1
+    }
+    return newField
+  }, [field, timer])
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      const time = timer + 1
+      setTimer(time)
+      // console.log(timer)
+      setField(blockDown)
+      blockRow === 16 ? setBlockRow(0) : setBlockRow(blockRow + 1)
+    }, 1000)
+    return () => {
+      clearTimeout(timeoutId)
+    }
   }, [timer])
   return (
     <Container>
@@ -90,7 +107,7 @@ const Home: NextPage = () => {
         <Grid>
           {field.map((row, x) =>
             row.map((col, y) => (
-              <Area key={`${x}-${y}`}>
+              <Area key={`${x}-${y}`} val={field[x][y]}>
                 {x}, {y}
               </Area>
             ))
@@ -100,5 +117,4 @@ const Home: NextPage = () => {
     </Container>
   )
 }
-
 export default Home
